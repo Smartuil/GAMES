876a40b26ad5d193becdd21753203d74890b1377
Smartuil <563412673@qq.com>
2024-02-20 23:21:59 +0800

    GAMES101-Homework7

--- GAMES101/Homework7/Scene.cpp
+++ GAMES101/Homework7/Scene.cpp
@@ -60,5 +60,43 @@ bool Scene::trace(
 // Implementation of Path Tracing
 Vector3f Scene::castRay(const Ray &ray, int depth) const
 {
-    // TO DO Implement Path Tracing Algorithm here
+    // TODO Implement Path Tracing Algorithm here
+    Vector3f l_indir{ 0.f };
+    Vector3f l_dir{0.f};
+    Intersection ray_inter = intersect(ray);
+    if (!ray_inter.happened) {
+        return l_dir + l_indir;
+    }
+
+    float pdf_light;
+    Intersection light_inter;
+    sampleLight(light_inter, pdf_light);
+
+    Vector3f N = ray_inter.normal;
+    Vector3f x = light_inter.coords;
+    Vector3f wo = ray.direction;
+    Vector3f p = ray_inter.coords;
+    Vector3f ws = (x - p).normalized();
+    // shoot a ray from p to x
+    Ray ray_light_to_p = Ray(p + EPSILON * N, ws);
+    auto rpx_inter = intersect(ray_light_to_p);
+    Vector3f NN = rpx_inter.normal;
+    Material* m = ray_inter.m;
+    // if the ray is not blocked in the middle
+    if(rpx_inter.happened && rpx_inter.m->hasEmission()){
+        l_dir = rpx_inter.m->getEmission() * m->eval(wo, ws, N) * dotProduct(ws, N) \
+            * dotProduct(-ws, NN) / (rpx_inter.distance) / pdf_light;
+    }
+
+    // Test Russian Roulette with probability RussianRoulette
+    if (get_random_float() <= RussianRoulette) {
+        Vector3f wi = (m->sample(wo, N)).normalized();
+        Ray rpwi(p, wi);
+        auto rpwi_inter = intersect(rpwi);
+        if (rpwi_inter.happened && !rpwi_inter.m->hasEmission()) {
+            l_indir = castRay(rpwi, depth + 1) * m->eval(wo, wi, N) * dotProduct(wi, N) \
+                / m->pdf(wo, wi, N) / RussianRoulette;
+        }
+    }
+    return m->getEmission() + l_dir + l_indir;
 }
